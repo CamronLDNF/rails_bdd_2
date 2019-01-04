d0dd214846e463871482214939dbe45f81b94b18
camron <camronmoradi@gmail.com>
2019-01-04 19:01:13 +0100

    Updates feature file, adds Articles model, adds Rspec unit test to validate Articles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_144804) do
+ActiveRecord::Schema.define(version: 2019_01_04_175433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,15 +22,4 @@ ActiveRecord::Schema.define(version: 2018_11_25_144804) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string "name"
-    t.text "comment"
-    t.bigint "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email"
-    t.index ["article_id"], name: "index_comments_on_article_id"
-  end
-
-  add_foreign_key "comments", "articles"
 end
