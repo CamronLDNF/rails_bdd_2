dc6b6a6fb5ef3c573cdee87d60a530ac0dcfee64
camron <camronmoradi@gmail.com>
2019-01-04 21:03:43 +0100

    Adds create and new methods in Articles controller, add Articles resources, creates form or new Article

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   get 'articles/index'
+  # this tells Rails to map requests to 
+  # http://localhost:3000/articles/index to the 
+  # articles controller's index action.
+
+  resources :articles
+
   root controller: :articles, action: :index
+  # this tells Rails to map requests to the root of the 
+  # application to the articles controller's index action
+
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -13,7 +13,7 @@ When("I click {string}") do |link|
 end
 
 When("I fill in {string} with {string}") do |field, value|
-  
+  fill_in field, with: value
 end
 
 When("I click {string} button") do |string|
